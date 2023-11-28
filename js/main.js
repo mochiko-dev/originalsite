6221f0cd01550436c1b724db4b92cf5a83499d5c
mochiko-dev <152132598+mochiko-dev@users.noreply.github.com>
2023-11-29 01:44:20 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function () {
+  /*=================================================
+  ハンバーガーメニュー
+  ===================================================*/
+  // ハンバーガーメニューのクリックイベント
+  $(".hamburger").on("click", function () {
+    // headerにopenクラスがあるか判定する
+    if ($("header").hasClass("open")) {
+      // headerにopenクラスが存在しない場合、openクラスを削除する
+      $("header").removeClass("open");
+    } else {
+      // headerにopenクラスが存在する場合、openクラスを加える
+      $("header").addClass("open");
+    }
+  });
+
+  $('.mask').on('click', function () {
+    // openクラスを削除して、メニューを閉じる
+    $('header').removeClass('open');
+  });
+
+
+  $('.title').on('click', function() {//タイトル要素をクリックしたら
+    var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+    $(findElm).slideToggle();//アコーディオンの上下動作
+      
+    if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+      $(this).removeClass('close');//クラス名を除去し
+    }else{//それ以外は
+      $(this).addClass('close');//クラス名closeを付与
+    }
+  });
+
+  
+});
+
+function fadeAnime(){
+
+  // ふわっ
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+    }
+    });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
